6a01bf7a1cad7e85cbf32691d0383fccda90ddc5
Brabechonza <134477369+Brabechonza@users.noreply.github.com>
2026-01-26 20:49:41 +0100

    Delete click-it-web directory

--- click-it-web/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  const path = (location.pathname.split("/").pop() || "index.html").toLowerCase();
-  document.querySelectorAll("[data-nav]").forEach(a => {
-    const href = (a.getAttribute("href") || "").toLowerCase();
-    if ((path === "" && href.includes("index.html")) || href.endsWith(path)) {
-      a.classList.add("active");
-    }
-  });
-})();
